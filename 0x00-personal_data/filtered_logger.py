1b443b37d3afbc3ac79111a680d28a657487f619
jamesbright <jamesononiwu@gmail.com>
2023-08-30 14:24:08 +0100

    add connect to db

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,8 +6,8 @@ Filter personal information using a Regex.
 import re
 from typing import List
 import logging
-
-import logging
+import os
+import mysql.connector
 
 
 class RedactingFormatter(logging.Formatter):
@@ -67,3 +67,16 @@ def get_logger() -> logging.Logger:
 
     logger.addHandler(target_handler)
     return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """
+    Connect to mysql server with environmental vars
+    """
+    db_connect = mysql.connector.connect(
+        user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
+        password=os.getenv('PERSONAL_DATA_DB_PASSWORD', ''),
+        host=os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
+        database=os.getenv('PERSONAL_DATA_DB_NAME')
+    )
+    return db_connect
