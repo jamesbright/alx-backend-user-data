99c5538869a484d6ca6913876fcfa16a5fdc6119
jamesbright <jamesononiwu@gmail.com>
2023-08-30 14:00:29 +0100

    implemented get logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -44,3 +44,26 @@ def filter_datum(fields: List[str],
         message = re.sub(item + '=.*?' + separator, item + '=' +
                          redaction + separator, message)
     return message
+
+PII_FIELDS = ('name', 'email', 'password', 'ssn', 'phone')
+
+
+
+def get_logger() -> logging.Logger:
+    """
+    Returns a Logger object with a
+    StreamHandler with RedactingFormatter
+    as formatter
+    """
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    target_handler = logging.StreamHandler()
+    target_handler.setLevel(logging.INFO)
+
+    formatter = RedactingFormatter(list(PII_FIELDS))
+    target_handler.setFormatter(formatter)
+
+    logger.addHandler(target_handler)
+    return logger
