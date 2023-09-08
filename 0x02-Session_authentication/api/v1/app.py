7b41553d8ebc81bd87c18e05ae5ce7afb28ac642
jamesbright <jamesononiwu@gmail.com>
2023-09-08 14:29:33 +0100

    fixed request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -75,8 +75,6 @@ def before_request() -> str:
             if request.current_user is None:
                 abort(403)
 
-    request.current_user = auth.current_user(request)
-
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
