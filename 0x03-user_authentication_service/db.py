3fca4608900aaa99a86522a7a9bc89d89162e418
jamesbright <jamesononiwu@gmail.com>
2023-09-13 14:40:05 +0100

    add user to db

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -40,6 +40,9 @@ class DB:
             Return:
                 Saved user object
         """
+        if email is None or hashed_password is None:
+            return None
+        
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
