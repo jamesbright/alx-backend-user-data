dc9a89fa04fcb25a178ed2a50c1cf161a2602212
jamesbright <jamesononiwu@gmail.com>
2023-09-13 15:06:42 +0100

    implement find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -44,3 +46,26 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """ Finds a user using supplied arguments
+            Args:
+                **kwargs: search terms
+            Return:
+                User if found or raise exception
+        """
+        if kwargs is None:
+            raise InvalidRequestError
+        
+        for i in kwargs.keys():
+            if i not in User.__table__.columns.keys():
+                raise InvalidRequestError
+            
+        user_data = self._session.query(User).filter_by(**kwargs).first()
+
+        if user_data is None:
+            raise NoResultFound
+        
+        return user_data
+
+
